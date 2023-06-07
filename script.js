e5e72bd2f1fb8e7f9a6bb1e86d7e81dc20e166d1
Gustavo Souza <gustavoelia7@gmail.com>
2023-06-07 20:48:40 -0300

    updates: win

--- script.js
+++ script.js
@@ -27,40 +27,6 @@ function duckMove() {
 
 duckMove();
 
-// shot animation
-
-/*function shotMove(cursorX, cursorY) {
-    shotAnimation = setInterval(() => { anim(cursorX, cursorY) }, 1);
-    function anim(x, y) {
-        let aux = 0;
-
-        if(shotLeft != x-2 && shotLeft != x-1 && shotLeft != x && shotLeft != x+1 && shotLeft != x+2) {
-            if(shotLeft > x) shotLeft-=3; else shotLeft+=3;
-            c('.shot').style.left = shotLeft + "px";
-            verifyWin();
-        } else {
-            aux++;
-        }
-
-        if(shotTop != y-2 && shotTop != y-1 && shotTop != y && shotTop != y+1 && shotTop != y+2) {
-            if(shotTop > y) shotTop-=3; else shotTop+=3;
-            c('.shot').style.top = shotTop + "px";
-            verifyWin();
-        } else {
-            aux++;
-        }
-
-        if(aux == 2) {
-            clearInterval(shotAnimation);
-            shotAnimation = null;
-            shotLeft = initialShotLeft;
-            shotTop = initialShotTop;
-            c('.shot').style.left = shotLeft + "px";
-            c('.shot').style.top = shotTop + "px";
-        }
-    }
-}*/
-
 // modify a CSS variable
 function setCSSVariable(variable, value) {
     c(':root').style.setProperty(variable, value);
@@ -69,14 +35,14 @@ function setCSSVariable(variable, value) {
 // shoot the duck
 let initialShotLeft = parseInt((gameWidth/2) - 7);
 let initialShotTop = parseInt(gameHeight * 0.8);
-let finalShotLeft, finalShotTop, distance, animationTime, shotAnimInterval;
+let finalShotLeft, finalShotTop, distance, animationTime, shotAnimInterval, shotAnimClearInterval;
 c('.shot').style.left = initialShotLeft + "px";
 c('.shot').style.top = initialShotTop + "px";
 function shoot() {
     finalShotLeft = event.clientX-26;
     finalShotTop = event.clientY-26;
-    distance = initialShotTop - finalShotTop;
-    animationTime = distance / 640;
+    distance = Math.sqrt((initialShotLeft-finalShotLeft)**2 + (initialShotTop-finalShotTop)**2);
+    animationTime = distance / 800;
 
     setCSSVariable('--initialLeft', (initialShotLeft)+'px');
     setCSSVariable('--initialTop', (initialShotTop)+'px');
@@ -87,20 +53,25 @@ function shoot() {
 
     shotAnimInterval = setInterval(verifyWin, 5);
 
-    setTimeout(() => {
-        c('.shot').style.left = initialShotLeft + "px";
-        c('.shot').style.top = initialShotTop + "px";
-        c('.shot').classList.remove('shotAnimation');
-        clearInterval(shotAnimInterval);
-    }, animationTime*1000);
+    shotAnimClearInterval = () => {
+        setTimeout(() => {
+            c('.shot').style.left = initialShotLeft + "px";
+            c('.shot').style.top = initialShotTop + "px";
+            c('.shot').classList.remove('shotAnimation');
+            clearInterval(shotAnimInterval);
+        }, animationTime*1000);
+    }
+
+    shotAnimClearInterval();
 }
 
 function verifyWin() {
-    if(parseInt(c('.shot').style.left) >= parseInt(c('.duck').style.left) && 
-    parseInt(c('.shot').style.left) <= parseInt(c('.duck').style.left)+70-15 && 
-    parseInt(c('.shot').style.top) >= parseInt(c('.duck').style.top) && 
-    parseInt(c('.shot').style.top) <= parseInt(c('.duck').style.top)+70-15) {
+    let duck = c('.duck').getBoundingClientRect();
+    let shot = c('.shot').getBoundingClientRect();
+    if(shot.left >= duck.left && shot.left <= duck.left+70-15 
+        && shot.top >= duck.top && shot.top <= duck.top+70-15) {
         clearInterval(duckAnimation);
+        shotAnimClearInterval();
     }
 }
 
