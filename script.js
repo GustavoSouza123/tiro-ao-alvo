4b0d6b7c865c5ac8a128d1409cd2fe13b100d6e0
Gustavo Souza <gustavoelia7@gmail.com>
2023-06-06 21:41:56 -0300

    duck move, win detector

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 const c = (e) => document.querySelector(e);
 const cs = (e) => document.querySelectorAll(e);
 
+// duck move
+let duckAnimation;
+function duckMove() {
+    let initialPos = -100;
+    let left = initialPos;
+    c('.duck').style.left = left + "px";
+    c('.duck').style.top = (window.innerHeight/4.5) + "px";
+    animation = setInterval(anim, 2);
+    function anim() {
+        if(left == window.innerWidth) {
+            left = initialPos;
+            c('.duck').style.left = left + "px";
+        } else {
+            left++;
+            c('.duck').style.left = left + "px";
+        }
+    }
+}
+
+duckMove();
+
+// shoot the duck
 function shoot() {
     c('.shot').style.top = event.clientY - 26 + "px";
     c('.shot').style.left = event.clientX - 26 + "px";
+
+    if(parseInt(c('.shot').style.left) >= parseInt(c('.duck').style.left) && 
+    parseInt(c('.shot').style.left) <= parseInt(c('.duck').style.left)+70-15 && 
+    parseInt(c('.shot').style.top) >= parseInt(c('.duck').style.top) && 
+    parseInt(c('.shot').style.top) <= parseInt(c('.duck').style.top)+70-15) {
+        clearInterval(duckAnimation);
+    }
 
     console.log("cursor:")
     console.log(event.clientX);
@@ -16,5 +45,12 @@ function shoot() {
     console.log(c('.duck').style.left);
     console.log(c('.duck').style.top);
 }
+
+
+/*
+let hit = false;
+while(!hit) {
+
+}*/
 
 c('body').addEventListener('mousedown', shoot);
